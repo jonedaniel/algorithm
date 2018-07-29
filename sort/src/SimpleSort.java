1ee1d46ec57b5b89e7e0be373b49cd06a1192ebc
zmh <623299342@qq.com>
2018-07-30 00:02:19 +0800

    almost sort

--- sort/src/SimpleSort.java
+++ sort/src/SimpleSort.java
@@ -1,12 +1,72 @@
 import java.util.Arrays;
 
+/**
+ * revise sort
+ *
+ * @author zmh email: zmhzhaomenghui@gmail.com
+ * @createDate 2018/7/27
+ * @see "https://www.jianshu.com/p/8c915179fd02"
+ */
 public class SimpleSort {
     static int[] arr = {2, 3, 1, 2, 5, 4, 9, 7, 6};
 
     public static void main(String[] args) {
+        print();
+
 //        bubble();
 //        choice();
-        binary();
+//        binary();
+        qsort(arr, 0, arr.length-1);
+//        shellSort();
+
+
+        print();
+    }
+
+    public static void shellSort() {
+        int j    = 0;
+        int temp = 0;
+        //每次将步长缩短为原来的一半
+        for (int increment = arr.length / 2; increment > 0; increment /= 2) {
+            for (int i = increment; i < arr.length; i++) {
+                temp = arr[i];
+                for (j = i; j >= increment; j -= increment) {
+                    if (temp < arr[j - increment])//如想从小到大排只需修改这里
+                    {
+                        arr[j] = arr[j - increment];
+                    } else {
+                        break;
+                    }
+
+                }
+                arr[j] = temp;
+            }
+
+        }
+    }
+
+    //快排1
+    private static void qsort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);        //将数组分为两部分
+            qsort(arr, low, pivot - 1);                   //递归排序左子数组
+            qsort(arr, pivot + 1, high);                  //递归排序右子数组
+        }
+    }
+
+    //快排2
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];     //枢轴记录
+        while (low < high) {
+            while (low < high && arr[high] >= pivot) --high;
+            arr[low] = arr[high];             //交换比枢轴小的记录到左端
+            while (low < high && arr[low] <= pivot) ++low;
+            arr[high] = arr[low];           //交换比枢轴小的记录到右端
+        }
+        //扫描完成，枢轴到位
+        arr[low] = pivot;
+        //返回的是枢轴的位置
+        return low;
     }
 
     static void print() {
@@ -30,9 +90,9 @@ public class SimpleSort {
 
     private static void choice() {
         print();
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i; j < arr.length; j++) {
-                if (arr[i]>arr[j]) {
+                if (arr[i] > arr[j]) {
                     int m = arr[i];
                     arr[i] = arr[j];
                     arr[j] = m;
@@ -46,8 +106,8 @@ public class SimpleSort {
         print();
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
-            int low = 0, high = i - 1;
-            int mid = -1;
+            int low  = 0, high = i - 1;
+            int mid  = -1;
             while (low <= high) {
                 mid = low + (high - low) / 2;
                 if (arr[mid] > temp) {
@@ -56,9 +116,7 @@ public class SimpleSort {
                     low = mid + 1;
                 }
             }
-            for(int j = i - 1; j >= low; j--) {
-                arr[j + 1] = arr[j];
-            }
+            if (i - low >= 0) System.arraycopy(arr, low, arr, low + 1, i - low);
             arr[low] = temp;
         }
         print();

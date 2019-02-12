a9cad74f2d2351e2e0d9b2123ffb949f4b53d1f6
zmh2 <zmh.infomation@gmail.com>
2019-02-12 16:12:51 +0800

    quick sort by middle value

--- algorithm/src/sort/Quicksort.java
+++ algorithm/src/sort/Quicksort.java
@@ -1,8 +1,6 @@
 package sort;
 
-import java.security.PublicKey;
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * Title: Quicksort
@@ -14,34 +12,66 @@ import java.util.Random;
  */
 public class Quicksort {
 
-    int[] originalArr = new int[]{8,9,4,1,5,0};
 
     public static void main(String[] args) {
         Quicksort qs = new Quicksort();
-        System.out.println(Arrays.toString(qs.originalArr));
-        qs.quicksort(qs.originalArr, 0, qs.originalArr.length - 1);
-        System.out.println(Arrays.toString(qs.originalArr));
+        System.out.println(Arrays.toString(originalArr));
+        qs.quicksortMiddle(originalArr, 0, originalArr.length - 1);
+        System.out.println(Arrays.toString(originalArr));
     }
 
     private void quicksort(int[] arr, int low, int high) {
         if (low < high) {
             int pivot = partition(arr, low, high);
-            quicksort(arr, low, pivot-1);
-            quicksort(arr, pivot+1, high);
+            quicksort(arr, low, pivot - 1);
+            quicksort(arr, pivot + 1, high);
         }
     }
 
-    private int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];
-        while (low < high) {
-            while (low < high && arr[high] >= pivot) --high;
-            arr[low] = arr[high];
-            while (low < high && arr[low] <= pivot ) ++low;
-            arr[high] = arr[low];
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+
+        for (int i = start; i < end; i++) {
+            if (arr[i] < pivot) {
+                int temp = arr[start];
+                arr[start] = arr[i];
+                arr[i] = temp;
+                start++;
+            }
         }
-        arr[low] = pivot;
-        return low;
+
+        int temp = arr[start];
+        arr[start] = pivot;
+        arr[end] = temp;
+
+        System.out.println(Arrays.toString(originalArr));
+        return start;
     }
 
+    private void quicksortMiddle(int[] arr, int low, int high) {
+        if (arr == null || arr.length == 0) return;
+        if(high < low) return;
+
+        int middle = low + (high-low)/2;
+        int pivot = arr[middle];
+
+        int i = low, j = high;
+        while (i <= j) {
+            while(arr[i] < pivot)i++;
+            while (arr[j]>pivot)j--;
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if(low < j) quicksortMiddle(arr,low,j);
+        if(high > i) quicksortMiddle(arr,i,high);
+    }
+
+    static int[] originalArr = new int[]{8, 9, 4, 1, 5, 0};
 
 }

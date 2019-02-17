1f76dfb5433094cd9629a695461d6749025f040b
zmh_new <zhaomenghui93@163.com>
2019-02-17 22:04:40 +0800

    correct

--- algorithm/src/search/BinarySearch.java
+++ algorithm/src/search/BinarySearch.java
@@ -7,11 +7,11 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         print(arr);
-        System.out.println(new BinarySearch().search(arr,6,0,arr.length-1));
+        System.out.println(new BinarySearch().search(arr,1,0,arr.length-1));
     }
 
     private int search(int[] arr, int v,int start,int end) {
-        if(end <= start) return -1;
+        if (end < start) return -1;
         int mid = (end -start)/2+start;
         if(arr[mid]==v) return mid;
         else if (arr[mid] > v) return search(arr, v, start, mid - 1);

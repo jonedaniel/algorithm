04568908b1802307c0137213ec44180d1a564652
zhaomenghui <zhaomenghui@liepin.com>
2019-05-14 15:14:24 +0800

    test

---
+++ algorithm/src/leetcode/easy/AlientDictionary.java
@@ -0,0 +1,12 @@
+package leetcode.easy;
+
+/**
+ * 某种外星语也使用英文小写字母，但可能顺序 order 不同。字母表的顺序（order）是一些小写字母的排列。
+ *
+ * 给定一组用外星语书写的单词 words，以及其字母表的顺序 order，只有当给定的单词在这种外星语中按字典序排列时，返回 true；否则，返回 false。
+ * 
+ * @since 2019/5/8
+ */
+public class AlientDictionary {
+
+}
--- algorithm/test/TestJunit.java
+++ algorithm/test/TestJunit.java
@@ -17,4 +17,7 @@ public class TestJunit {
         hello();
         assertEquals(1, test1());
     }
+
+
+
 }

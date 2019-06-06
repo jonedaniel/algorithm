4db8939e09bc86004316d63de5453b81cde63dbd
zhaomenghui <zhaomenghui@liepin.com>
2019-06-06 17:33:32 +0800

    twoSum

---
+++ algorithm/src/leetcode/easy/TwoSum.java
@@ -0,0 +1,36 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * 
+ * @author zhaomh
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new TwoSum().twoSum(new int[]{2,7,11,15},9)));
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+}
